ee45dc3c8b14024ece9aa222aebb9902a1219c56
Sam Harke <97071381@st.deltion.nl>
2020-03-17 11:11:12 +0100

    Initiele project (2)

---
+++ BasicWebProject/App_Code/Songclass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+
+namespace BasicWebProject.App_Code
+{
+    public class Songclass
+    {
+        public DataSet getAllSongs()
+        {
+            DataSet ds = new DataSet("playlist");
+            DataTable dt = new DataTable("song");
+            DataColumn dcId = new DataColumn("id");
+            DataColumn dcArtist = new DataColumn("artist");
+            DataColumn dcTitle = new DataColumn("title");
+
+            dt.Columns.Add(dcId);
+            dt.Columns.Add(dcArtist);
+            dt.Columns.Add(dcTitle);
+            ds.Tables.Add(dt);
+
+            ds.ReadXml(HttpContext.Current.Server.MapPath("App_Data/playlist.xml"));
+        }
+    }
+}
