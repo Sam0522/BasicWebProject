f7f8977a2aabf568fca8758620c35a10b94976c7
Sam Harke <97071381@st.deltion.nl>
2020-04-21 11:39:40 +0200

    Update Zam

---
+++ BasicWebProject/App_Code/Song.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+namespace BasicWebProject.App_Code
+{
+    public class Song
+{
+    public string id { get; set; }
+    public string artist { get; set; }
+    public string title { get; set; }
+    public string year { get; set; }
+}
+}
